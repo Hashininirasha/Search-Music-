860987bf2cbcc103e19b6392e3fdb862a568428d
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-08 22:47:25 +0530

    created keypress function

---
+++ src/Components/Resultarea.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Results from ResultArea Component</h2>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/Components/Searcharea.vue
+++ src/Components/Searcharea.vue
@@ -2,13 +2,18 @@
     <div class="form-group">
             <label for="inputEmail" class="col-lg-2 control-label">Search Music</label>
                     <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputEmail" placeholder="Type here">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Type here" @input="keypress">
                     </div>
     </div>
 </template>
 
 <script>
 export default {
+    methods:{
+        keypress(){
+            
+        }
+    }
     
 
 }
