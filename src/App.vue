bd377a7960f2a9fe6d734e0c6a077434ca9881f7
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-08 19:57:44 +0530

    added home

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
      
     <app-Navbar></app-Navbar>
+    <app-home></app-home>
 
     
   </div>
 </template>
 
 <script>
-
+import Home from './Components/Home.vue'
 export default {
  
-
+components:{
+  'app-home':Home
+}
 }
 </script>
 
